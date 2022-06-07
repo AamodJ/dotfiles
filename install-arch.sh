f1d76332707c471a84e214e67d39d24e74a66266
AamodJ <aamodj23@gmail.com>
2022-06-07 18:31:44 +0530

    Adding install script

---
+++ install-arch.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+echo "Installing yay"
+mkdir ~/git-clones
+cd ~/git-clones
+git clone https://aur.archlinux.org/yay.git
+cd yay 
+makepkg -si
+echo "yay installation complete!"
+cd ..
+rm -rf yay 
+sleep 3
+echo "Updating yay databases"
+yay -Syu 
+echo "Making sure git is configured"
+git config --global user.email "aamodj23@gmail.com"
+git config --global user.name "AamodJ"
+git config --global credential.helper store
+echo "Git configured"
+cd dotfiles 
+echo "Installing dependencies"
+yay -S --noconfirm bspwm sxhkd polybar kitty dunst neofetch lf rclone rofi gotop nsxiv pandoc-bin vundle bat feh obs-studio pipewire-pulse udisks2 simple-mtpfs zathura zathura-djvu zathura-pdf-mupdf mbpfan spaceship-prompt-git picom-ibhagwan-git brightnessctl zsh-syntax-highlighting fzf man cpupower-gui scrot firefox betterlockscreen pipewire-jack pipewire-alsa qjackctl autocutsel cronie 
+echo "Copying configs"
+cp -r .config/* ~/.config/
+mkdir ~/.dotfiles
+cp -r .dotfiles/* ~/.dotfiles
+mkdir -p ~/.local
+cp -r .local/* ~/.local/
+cp .Xresources ~/.Xresources
+cp .xinitrc ~/.xinitrc 
+cp .zprofile ~/.zprofile 
+cp .zshenv ~/.zshenv
+cd ~/.dotfiles/dmenu 
+makepkg -si
+mkdir ~/.config/z
+touch ~/.config/z/.z
+mkdir ~/Pictures/Screenshots
+betterlockscreen -u ~/.dotfiles/bg.jpg
+echo "Configured!"
+sleep 3
+echo "Installing timeshift"
+cd ~/git-clones
+git clone https://aur.archlinux.org/timeshift.git
+cd timeshift
+makepkg -si
+echo "Timeshift installed!"
+cd .. 
+rm -rf timeshift
+echo "Fixing audio"
+sudo cp ~/.config/audio/AppleT2.conf /usr/share/alsa/cards/AppleT2.conf
+sudo cp ~/.config/audio/apple-t2.conf /usr/share/alsa-card-profile/mixer/profile-sets/apple-t2.conf
+sudo cp ~/.config/audio/91-pulseaudio-custom.rules /usr/lib/udev/rules.d/91-pulseaudio-custom.rules
+echo "Audio fixed!"
+sleep 3
+echo "Fixing fan"
+git clone https://github.com/networkException/mbpfan
+cd mbpfan
+make
+sudo make tests
+sudo make install
+sudo cp mbpfan.service /etc/systemd/system/
+sudo systemctl enable mbpfan.service
+sudo systemctl daemon-reload
+sudo systemctl start mbpfan.service
+cd ..
+rm -rf mbpfan
+echo "Fan Fixed!"
+sleep 3
+echo "Installation complete. For changes to take effect, reboot."
+echo "Installing zsh and making it default shell"
+yay -S --noconfirm zsh 
+chsh -s /usr/bin/zsh
