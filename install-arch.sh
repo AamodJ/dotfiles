28712a5b9daa46ab4d15acae032f55389f43d39b
AamodJ <aamodj23@gmail.com>
2022-06-07 19:12:55 +0530

    Updating install script with little bit of documentation

--- install-arch.sh
+++ install-arch.sh
@@ -12,14 +12,19 @@ rm -rf yay
 sleep 3
 echo "Updating yay databases"
 yay -Syu 
+
+# Remove these lines or change them if you wish to 
 echo "Making sure git is configured"
 git config --global user.email "aamodj23@gmail.com"
 git config --global user.name "AamodJ"
 git config --global credential.helper store
 echo "Git configured"
+
 cd dotfiles 
+
 echo "Installing dependencies"
 yay -S --noconfirm bspwm sxhkd polybar kitty dunst neofetch lf rclone rofi gotop nsxiv pandoc-bin vundle bat feh obs-studio pipewire-pulse udisks2 simple-mtpfs zathura zathura-djvu zathura-pdf-mupdf mbpfan spaceship-prompt-git picom-ibhagwan-git brightnessctl zsh-syntax-highlighting fzf man cpupower-gui scrot firefox betterlockscreen pipewire-jack pipewire-alsa qjackctl autocutsel cronie 
+
 echo "Copying configs"
 cp -r .config/* ~/.config/
 mkdir ~/.dotfiles
@@ -34,10 +39,14 @@ cd ~/.dotfiles/dmenu
 makepkg -si
 mkdir ~/.config/z
 touch ~/.config/z/.z
+
+# In sxhkdrc, scrot saves the screenshots to ~/Pictures/Screenshots, so we need to create the Screenshots folder 
 mkdir ~/Pictures/Screenshots
-betterlockscreen -u ~/.dotfiles/bg.jpg
+
 echo "Configured!"
 sleep 3
+
+# Remove this part if you wish to use another system backup tool (eg. snapper)
 echo "Installing timeshift"
 cd ~/git-clones
 git clone https://aur.archlinux.org/timeshift.git
@@ -46,6 +55,8 @@ makepkg -si
 echo "Timeshift installed!"
 cd .. 
 rm -rf timeshift
+
+# 2018 Macbook Pro specific things. Remove if irrelavent for your machine
 echo "Fixing audio"
 sudo cp ~/.config/audio/AppleT2.conf /usr/share/alsa/cards/AppleT2.conf
 sudo cp ~/.config/audio/apple-t2.conf /usr/share/alsa-card-profile/mixer/profile-sets/apple-t2.conf
@@ -65,6 +76,7 @@ sudo systemctl start mbpfan.service
 cd ..
 rm -rf mbpfan
 echo "Fan Fixed!"
+
 sleep 3
 echo "Installation complete. For changes to take effect, reboot."
 echo "Installing zsh and making it default shell"
